f527b1357abaad8977a768f171d8a86f20dc6d38
kaiky <kaikycintra@gmail.com>
2026-05-04 21:04:15 -0300

    feat: implements cyclist thread logic, although with many glitches

--- ep2.c
+++ ep2.c
@@ -20,7 +20,7 @@
 #define QUANTUM 60 // intervalo de tempo em ms que define um passo da simulação
 
 pthread_mutex_t mutex_velodromo; // usado para implementação ineficiente
-pthread_cond_t sinal_arbitro;
+pthread_cond_t sinal_arbitro, sinal_destruicao;
 
 typedef struct {
     int pista;
@@ -37,6 +37,7 @@ typedef struct {
     int pontos;
     int volta_atual;
     bool terminou;
+    bool revezando;
 } struct_equipe;
     
 typedef struct {
@@ -51,6 +52,7 @@ typedef struct {
 
 typedef struct {
     struct_ciclista *ciclista;
+    struct_ciclista *colega;
     struct_equipe *equipe;
     int voltas;
     int comprimento_velodromo;
@@ -107,8 +109,8 @@ void embaralha_ciclistas(struct_ciclista *c_ativos[],
     }
 }
 
-void printa_velodromo(char *velodromo[10][MAX_LEN_VELODROMO], int comprimento_velodromo, bool debug) {
-    FILE *saida = debug ? stderr : stdout;
+void printa_velodromo(char *velodromo[10][MAX_LEN_VELODROMO], int comprimento_velodromo, bool error_out) {
+    FILE *saida = error_out ? stderr : stdout;
 
     int linhas = 10;
     int colunas = comprimento_velodromo;
@@ -176,54 +178,198 @@ void posiciona_inicio_corrida(struct_corrida* corrida, bool debug) {
                         corrida->velodromo,
                         corrida->qtd_equipes,
                         corrida->comprimento_velodromo);
-    printa_velodromo(corrida->velodromo, corrida->comprimento_velodromo, debug);
+    //printa_velodromo(corrida->velodromo, corrida->comprimento_velodromo, debug);
 }
 
-void atualizar_posicao() {
+bool acabou_corrida(struct_equipe *equipes[], int qtd_equipes) {
+    for(int i = 0; i < qtd_equipes*2; i++) {
+        if(equipes[i]->terminou == false) return false;
+    }
+    return true;
+}
+
+bool pode_ultrapassar(int pista, int coluna, char *velodromo[10][MAX_LEN_VELODROMO]) {
+    for(int p = pista; p < QTD_PISTAS-1; p++) {
+        if(velodromo[p][coluna+1] == NULL) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void avanca_posicao(struct_ciclista *ciclista,
+                    char *velodromo[10][MAX_LEN_VELODROMO],
+                    int comprimento_pista) {
+    int coluna = ciclista->coluna;
+    int pista = ciclista->pista;
+    int coluna_antiga = coluna, pista_antiga = pista;
+
+    // se pode andar para a frente
+    if(velodromo[pista][coluna+1] == NULL) {
+        coluna = coluna+1;
+    }
+    
+    // se não, se pode ultrapassar
+    else if(pode_ultrapassar(pista, coluna, velodromo)) {
+        for(int p = pista; p < QTD_PISTAS-1; p++) {
+            if(velodromo[p][coluna+1] == NULL) {
+                pista = p;
+                coluna = coluna+1;
+                break;
+            }
+        }
+    }
+    // se não, espera alguém sair da frente (como pedido nas especificações)
+
+    coluna = (coluna == comprimento_pista) ? 0 : coluna; // faz corrida ser circular
+    velodromo[pista_antiga][coluna_antiga] = NULL;
+    velodromo[pista][coluna] = ciclista->nome;
+    ciclista->coluna = coluna;
+    ciclista->pista = pista;
+    ciclista->tempo_restante_para_andar = ciclista->tempo_por_volta;
+}
+
+void reposiciona_pista(struct_ciclista *ciclista,
+                      char *velodromo[10][MAX_LEN_VELODROMO]) {
+    // se reposiciona de pista, ciclistas ativos devem ir às pistas internas, outros às pistas externas
+    int coluna = ciclista->coluna;
+    int pista = ciclista->pista;
+    int pista_antiga = pista;
+    char *estado = ciclista->estado;
+
+    if(strcmp(estado, "ativo") == 0) {
+        for(int p = pista; p >= 0; p--) {
+            if(velodromo[p][coluna] == NULL) {
+                pista = p;
+            }
+        }
+    }
+    else {
+        for(int p = pista; p <= QTD_PISTAS; p++) {
+            if(velodromo[p][coluna] == NULL) {
+                pista = p;
+            }
+        }
+    }
+
+    velodromo[pista_antiga][coluna] = NULL;
+    velodromo[pista][coluna] = ciclista->nome;
+    ciclista->pista = pista;
+}
+
+void atualiza_velocidade(struct_ciclista *ciclista, int velocidade_colega, bool revezando) {
+    if(revezando) {
+        ciclista->tempo_por_volta = strcmp(ciclista->estado, "ativo") == 0 ? ciclista->tempo_por_volta : velocidade_colega;
+        return;
+    }
+
+    if(strcmp(ciclista->estado, "descansando") == 0) {
+        ciclista->tempo_por_volta = 240;
+    }
+
+    if(ciclista->tempo_por_volta == 120) {
+        ciclista->tempo_por_volta = (rand() % 100 < 80) ? 60 : 120;
+    }
+    else if (ciclista->tempo_por_volta == 60) {
+        ciclista->tempo_por_volta = (rand() % 100 < 40) ? 60 : 120;
+    }
+    ciclista->tempo_restante_para_andar = ciclista->tempo_por_volta;
 }
 
 void* ciclista(void* arg) {
     struct_ciclista_arg* dados = (struct_ciclista_arg*) arg;
+    while(dados->equipe->terminou == false) {
+        pthread_mutex_lock (&mutex_velodromo);
+        pthread_cond_wait(&sinal_arbitro, &mutex_velodromo);
+        
+        int coluna_antiga = dados->ciclista->coluna;
+        // ou avança posição ou se reposiciona na pista (sem contar ultrapassagens)
+        if(dados->ciclista->tempo_restante_para_andar == 0) {
+            avanca_posicao(dados->ciclista, dados->velodromo, dados->comprimento_velodromo);
+        }
+        else {
+            reposiciona_pista(dados->ciclista, dados->velodromo);
+            dados->ciclista->tempo_restante_para_andar = dados->ciclista->tempo_restante_para_andar - 60;
+        }
+        int coluna_nova = dados->ciclista->coluna;
+        
+        // se completou uma volta como ciclista ativo
+        if(coluna_nova < coluna_antiga && strcmp(dados->ciclista->estado, "ativo") == 0) {
+            dados->equipe->volta_atual++;
+            dados->ciclista->voltas_realizadas++;
+            atualiza_velocidade(dados->ciclista, dados->colega->tempo_por_volta, dados->equipe->revezando);
 
-    // struct_ciclista* dados = (struct_ciclista*) arg;
-    // controla sua própria velocidade
-    //   caso a volta anterior tenha sido feita a 30Km/h, o sorteio é feito com 80%
-    //   de chance de escolher 60Km/h e 20% de chance de escolher 30Km/h. Caso a volta anterior tenha sido
-    //   feita a 60Km/h, o sorteio é feito com 40% de chance de escolher 60Km/h e 60% de chance de escolher 30Km/h
-    // controla quando deve avançar
-    // se está a 60km/h e há um na frente a 30km/h, anda a 30km/h se não consegue ultrapassar
-    // para ultrapassar deve haver espaço à frente em uma pista mais externa, movimentação entre pistas é instantânea
-    // revezamento acontece a partir de quando a cicilista ativa completa 5 voltas
-    //   a nova ciclista entra na prova com a mesma velocidade da ativa
-    //   a ciclista que entrou em recuperação vai para a pista mais externa e pedala a 15km/h
-    // atualiza sua posição no velódromo (seção crítica), remove identificador na posição antiga
-    pthread_mutex_lock (&mutex_velodromo);
-    atualizar_posicao();
-    pthread_mutex_unlock (&mutex_velodromo);
-    // atualiza a equipe, acabou a corrida
+            if(dados->ciclista->voltas_realizadas >= 5) {
+                dados->equipe->revezando = true;
+            }
+        }
+
+        // se está revezando, atualiza a própria velocidade
+        if(dados->equipe->revezando) {
+            atualiza_velocidade(dados->ciclista,
+                                dados->colega->tempo_por_volta,
+                                dados->equipe->revezando);
+        }
+
+        // se está revezando, verifica se pode trocar estado com colega de equipe
+        // o ciclista que está revezando 'informa' o colega que agora ele está ativo
+        if(dados->equipe->revezando && strcmp(dados->ciclista->estado, "descansando")==0) {
+            char *nome = dados->ciclista->nome;
+            for(int i = 0; i < QTD_PISTAS; i++) {
+                if(strncmp(dados->velodromo[i][dados->ciclista->coluna], nome, strlen(nome)-1) == 0) {
+                    sprintf(dados->ciclista->estado, "ativo");
+                    sprintf(dados->colega->estado, "descansando");
+                    dados->equipe->revezando = false;
+                }
+            }
+        }
+        
+        pthread_mutex_unlock (&mutex_velodromo);
+    }
+
+    pthread_cond_wait(&sinal_destruicao, &mutex_velodromo);
     return NULL;
 }
 
 void arbitro(struct_corrida* corrida, bool debug) {
-    if(corrida->volta_atual == 0) {
-        posiciona_inicio_corrida(corrida, debug);
-    }
+    posiciona_inicio_corrida(corrida, debug);
 
     pthread_mutex_init(&mutex_velodromo, NULL);
+    pthread_cond_init(&sinal_arbitro, NULL);
+    pthread_cond_init(&sinal_destruicao, NULL);
     pthread_t t_ciclista[corrida->qtd_equipes*2];
-    
+
     for(int i = 0; i < corrida->qtd_equipes*2; i++) {
         struct_ciclista_arg *arg_i = malloc(sizeof(struct_ciclista_arg));
         arg_i->ciclista = corrida->ciclistas[i];
-        arg_i->equipe = corrida->equipes[i];
+        arg_i->equipe = corrida->equipes[i/2];
         arg_i->voltas = corrida->voltas;
         arg_i->comprimento_velodromo = corrida->comprimento_velodromo;
         arg_i->velodromo = corrida->velodromo;
+        arg_i->colega = (i % 2 == 0) ? corrida->ciclistas[i+1] : corrida->ciclistas[i-1];
         pthread_create(&t_ciclista[i], NULL, ciclista, arg_i);
     }
 
-    pthread_cond_init(&sinal_arbitro, NULL);
-    pthread_cond_broadcast(&sinal_arbitro);
+    sleep(1); // dorme para garantir que todas as threads estão esperando sinal do árbitro
+    printa_velodromo(corrida->velodromo, corrida->comprimento_velodromo, debug);
+    while(acabou_corrida(corrida->equipes, corrida->qtd_equipes) == false) {
+        pthread_mutex_lock(&mutex_velodromo);
+        pthread_cond_broadcast(&sinal_arbitro); // sinal para threads rodarem, se não terminaram corrida
+        pthread_cond_broadcast(&sinal_destruicao); // sinal para threads encerrarem, se terminaram corrida
+        pthread_mutex_unlock(&mutex_velodromo);
+        if(debug) {
+            printa_velodromo(corrida->velodromo, corrida->comprimento_velodromo, true);
+        } 
+        sleep(QUANTUM/10);
+    }
+
+    for(int i = 0; i < corrida->qtd_equipes * 2; i++) {
+        pthread_join(t_ciclista[i], NULL);
+    }
+    
+    pthread_mutex_destroy(&mutex_velodromo);
+    pthread_cond_destroy(&sinal_arbitro);
+    pthread_cond_destroy(&sinal_destruicao);
     
     // versão ingênua, um semáforo para controlar acesso à matriz velódromo
     // versão eficiente, um semáforo para cada posição ou coluna da matriz
@@ -283,6 +429,7 @@ void gera_equipes(struct_equipe *equipes[MAX_EQUIPES], int qtd_equipes) {
         equipe_i->pontos = 0;
         equipe_i->volta_atual = 0;
         equipe_i->terminou = false;
+        equipe_i->revezando = false;
         equipes[i] = equipe_i;
     }
 }
